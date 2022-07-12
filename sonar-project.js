80a19f7cf14310ce422c7fdcc7d2de1825656a5e
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-07-12 16:30:02 +0900

    test

--- sonar-project.js
+++ sonar-project.js
@@ -4,9 +4,6 @@ sonarqubeScanner(
         serverUrl: "https://sonarcloud.io",
         options: {
             "sonar.sources": "./src",
-            "sonar.exclusions": "**/__tests__/**",
-            "sonar.tests": "./src/__tests__",
-            "sonar.test.inclusions": "./src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts",
             "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
             "sonar.testExecutionReportPaths": "reports/test-report.xml",
         },
